fef2b81c5d69b76225ae3d4eb8988b8710006364
Kenneth Clausen <contact@kjc.dk>
2021-11-30 14:25:53 +0100

    Created UI for DeepNotes

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deep(pink)Notes</title>
+</head>
+<body bgcolor="#ff1493">
+    <form method="post">
+        <input type="text" name="note">
+        <button type="submit">Gem</button>
+    </form>
+
+    <ul>
+        <li>Min test note</li>
+    </ul>
+</body>
+</html>
